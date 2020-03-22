2a1fac85a21aedf02012f57fa4b83b13be7dcf51
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-03-22 00:03:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,19 +1,19 @@
 class Owner
  attr_reader :name, :species
+ 
+ @@all = []
 
-@@all = []
  def initialize (name)
    @name = name
    @species = "human"
-   @@all << self
- end
+end
 
+ def species
+   "I am a #{species}."
+ end
  
  def self.all
    @@all
  end
  
- def self.count
-   @@all.count
- end
 end
